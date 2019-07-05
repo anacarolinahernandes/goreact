e04ad3617dc96b128ee63fb95dd92d33c86cb5e9
Ana Carolina Hernandes <ana.c.hernandes@accenture.com>
2019-07-05 17:21:38 -0300

    adding eslint, editorconfig and creating first components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
+import Button from './Button';
 
-class Button extends Component {
-    render() {
-        return <a href="">Salvar</a>
-    }
-}
+import './style.scss';
 
 class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>Hello, Rocketseat!</h1>
-                <Button />
-            </Fragment>
-        );
-    }
+  state = {
+    counter: 0,
+  };
+
+  // é necessário usar a arrow function para o this referenciar a classe e não diretamente à função
+  handleClick = () => {
+    // eslint-disable-next-line react/no-access-state-in-setstate
+    this.setState({ counter: (this.state.counter += 1) });
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <Fragment>
+        <h1 className="title">Hello, Rocketseat!</h1>
+        <h2>{counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
+      </Fragment>
+    );
+  }
 }
 
-render(<App />, document.querySelector("div#app"));
+render(<App />, document.querySelector('div#app'));
